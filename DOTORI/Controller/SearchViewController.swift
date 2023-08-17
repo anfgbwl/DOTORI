10a547ea42bd3c895dd5b9df0a9d86cba8538811
hidaehyunlee <hidaehyunlee@gmail.com>
2023-08-17 15:08:43 +0900

    feat searchVC ui

--- DOTORI/Controller/SearchViewController.swift
+++ DOTORI/Controller/SearchViewController.swift
@@ -7,23 +7,20 @@
 
 import UIKit
 
+// class SearchTableViewCell:
+
 class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var profile_name: UILabel!
+    @IBOutlet weak var profile_image: UIImageView!
+    @IBOutlet weak var profile_nickname: UILabel!
+    @IBOutlet weak var posting_time: UILabel!
+    @IBOutlet weak var posting_content: UILabel!
+    @IBOutlet weak var posting_contentimage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
